dc0596f6848725f28544cdfe8deaf27c762088c0
rafi-3 <nws04_01089114@pb.edu.bn>
2018-04-02 13:49:07 +0800

    clean up and fixed

--- ChatSession.java
+++ ChatSession.java
@@ -44,38 +44,10 @@ public class ChatSession implements Runnable {
 					+ " is connected to " + client2Name + " (" + 
 					client2IP + ").");
 			
-//			while(true) {
-//				if(sentMsgToClient1 == false) {
-//					toClient2.writeUTF(client1Name + " (" + client1IP 
-//							+ ") has disconnected.");
-//					toClient2.writeBoolean(sentMsgToClient1);
-//					break;
-//				}
-//				if(sentMsgToClient2 == false) {
-//					toClient1.writeUTF(client2Name + " (" + client2IP 
-//							+ ") has disconnected.");
-//					toClient1.writeBoolean(sentMsgToClient2);
-//					break;
-//				}
-//				if(fromClient1.available() > 0) {
-//					String msg = fromClient1.readUTF();
-//					try {
-//						toClient2.writeUTF(msg);
-//						toClient2.writeBoolean(sentMsgToClient1);
-//					} catch (SocketException e) {
-//						sentMsgToClient2 = false;
-//					}
-//				}
-//				if(fromClient2.available() > 0) {
-//					String msg = fromClient2.readUTF();
-//					try {
-//						toClient1.writeUTF(msg);
-//						toClient1.writeBoolean(sentMsgToClient2);
-//					} catch (SocketException e) {
-//						sentMsgToClient1 = false;
-//					}
-//				}
-//			}
+			int p1s = fromClient1.readInt();
+			int p2s = fromClient2.readInt();
+			toClient1.writeInt(p2s);
+			toClient2.writeInt(p1s);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
--- Client.java
+++ Client.java
@@ -12,7 +12,7 @@ public class Client {
 
 	static boolean chatActive = true;
 	static Scanner scanner = new Scanner(System.in);
-	static int receiveScore;
+	static int p2Score;
 	static int score;
 	static String[][] que = 
 		{
@@ -33,9 +33,6 @@ public class Client {
 			Socket socket = new Socket("localhost", 9101);
 			System.out.println("Connected to server.");
 			
-			
-			
-			
 			DataInputStream fromServer =
 					new DataInputStream(socket.getInputStream());
 			DataOutputStream toServer =
@@ -54,13 +51,22 @@ public class Client {
 					while(chatActive) {
 						try {
 							if(fromServer.available() > 0) {
-//								String receivedMsg = fromServer.readUTF();
-//								System.out.println(name +": " + receivedMsg);
 								
-								receiveScore = fromServer.readInt();
+								String receivestScore = fromServer.readUTF();
+								
+								p2Score = Integer.parseInt(receivestScore);
+								System.out.println("\nYour Score:" + score);
+								System.out.println("\nOponent Score:" + p2Score);
+								
+								if(score <p2Score) {
+									System.out.println("\nYou Lose!");
+								} else if(score == p2Score) {
+									System.out.println("\nDraw!");
+								} else {
+									System.out.println("\nYou Win!");
+								}
+								
 								chatActive = fromServer.readBoolean();
-								System.out.println(receiveScore);
-								compareScore();
 								}
 							} catch (IOException e) {
 							e.printStackTrace();
@@ -70,40 +76,47 @@ public class Client {
 				
 			}).start();
 			
+			
 			while(chatActive) {
-//				String sendMsg = scanner.nextLine();
-//				toServer.writeUTF(sendMsg);
-				
-				score= 0;
-				for(int i = 0; i < que.length; i++)	{
-					while(true) {
-						System.out.println(que[0][i]);
-						String userin = scanner.nextLine();
-						
-						if( i >= que.length) {
-							toServer.writeInt(score);
-							break;
-						}
-						
-						if(userin.equals(que[1][i])) {
-							if(i == que.length) {
-								System.exit(0);
-							}else {
-								score= score+10;
-								System.out.println("Current score" +score);
-								System.out.println("Next question:");
-								break;
+
+				try {
+					
+					for(int i = 0; i < que.length; i++) {
+						if( i < 2) {
+							while(true) {
+								System.out.println(que[0][i]);
+								String userin = scanner.nextLine();
+								
+								if(userin.equals(que[1][i])) {
+									System.out.println("correct!");
+									score = score+10;
+									break;
+								} else {
+									System.out.println("wrong!");
+									score = score-10;
+								}
 							}
-
-						}
-
-						else {
-							System.out.println("try again");
-							score = score-10;
-							System.out.println("Current score" +score);
 						}
 					}
+					
+					toServer.writeInt(score);
+					int newScore = fromServer.readInt();
+					System.out.println(score);
+					System.out.println(newScore);
+					if(score > newScore) {
+						System.out.println("You win");
+					} if (score == newScore) {
+						System.out.println("Its a draw!");
+					} if (newScore > score) {
+						System.out.println("You lose");
+					}
+
+					break;
+					
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
+				
 			}
 			
 		} catch (IOException e) {
@@ -111,20 +124,6 @@ public class Client {
 		}
 	}
 	
-	public static void compareScore() {
-		
-		System.out.println("\nYour Score:" + score);
-		System.out.println("\nOponent Score:" + receiveScore);
-		
-		if(score < receiveScore) {
-			System.out.println("\nYou Lose!");
-		} else if(score == receiveScore) {
-			System.out.println("\nDraw!");
-		} else {
-			System.out.println("\nYou Win!");
-		}
-		
-		int w = scanner.nextInt();
-	}
+	
 	
 }
